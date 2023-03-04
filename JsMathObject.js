0a27890db9337d36f2ea7eb287ed36b2bcd9ae08
Amit Pandey <access89480@gmail.com>
2023-03-04 22:34:36 +0530

    done

--- JsMathObject.js
+++ JsMathObject.js
@@ -13,4 +13,18 @@ console.log(Math.abs(-90));
 console.log(Math.floor(3.7));
 console.log(Math.ceil(3.2));
 
+console.log(Math.ceil(3.2));
+
+
+console.log(Math.max(90,12,5,63,456,0));
+console.log(Math.min(90,12,5,63,456,1));
+
+
+console.log(Math.floor(Math.random()*100));
+
+
+console.log(Math.trunc(-90.78))
+
+console.log(Math.trunc(91.23));
+
 
