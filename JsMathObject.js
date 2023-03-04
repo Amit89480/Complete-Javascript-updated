2da9ba2de839553d0917f01cb02a25a6809c0117
Amit Pandey <access89480@gmail.com>
2023-03-04 22:24:06 +0530

    done

---
+++ JsMathObject.js
@@ -0,0 +1,16 @@
+// In this we will be learning Mtahs.object
+
+
+console.log(Math.PI);
+console.log(Math.round(3.12));
+console.log(Math.pow(2,3));
+console.log(Math.sqrt(6));
+console.log(Math.sqrt(6));
+
+console.log(Math.abs(-90));
+
+
+console.log(Math.floor(3.7));
+console.log(Math.ceil(3.2));
+
+
