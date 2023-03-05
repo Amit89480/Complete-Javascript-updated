82babd88b9dd0d1d21d372deca3525f4cb6c0d57
Amit Pandey <access89480@gmail.com>
2023-03-06 00:15:10 +0530

    done

--- OOPS.js
+++ OOPS.js
@@ -176,10 +176,128 @@
 
 // object properties
 
+// let myName="amit";
+
+// const bioData={
+//     [myName]:myName,
+//    age:age
+// }
+
+// console.log(bioData)
+
+
+// let name="Amit"
+// let age=10
+
+// const myData={name,age};
+
+// console.log(myData)
+
+
+
+// spread opearors
+
+
+// const colors=['red','blue','orange'];
+
+// const upDated=['white','saffron',...colors]
+
+// console.log(upDated)
 
 
 
 
+// es7 features
+
+
+// 1. array includes
+
+
+// const color=['red','blue','black']
+// const ispresent=color.includes('red');
+
+// console.log(ispresent)
 
 
 
+// 2. exponential opeartor
+
+
+// console.log(2**2)
+
+
+
+// es8
+
+
+// string padding
+
+// const message="my name is amit";
+// console.log(message.padStart(5))
+// console.log(message.padEnd(10))
+
+
+
+// const person={name:"amit",age:90};
+
+// // console.log(Object.values(person))
+// console.log(Object.entries(person))
+
+
+
+// es2018
+
+// const person={name:"amit",age:90};
+
+// const sperson={...person}
+
+// console.log(person)
+
+// console.log(sperson)
+
+
+
+// es2019  flat()
+
+// const arr = [
+//     [1, 2, 3],
+//     [4, 5, 6],
+//     [7, 8,[3,4,5], 9]
+// ];
+
+// const flattenArray = arr.reduce((accu, currE, index, arr) => {
+//     return accu.concat(currE);
+// })
+
+
+// console.log(arr.flat(Infinity))
+
+// const person={name:"amit",age:90};
+
+// const arrObj=Object.entries(person)
+
+
+// console.log(arrObj)
+// console.log(Object.fromEntries(arrObj))
+
+
+// es2020
+// bigint
+//  oldnum=Number.MAX_SAFE_INTEGER;
+// let num=9007199254741000n+15n;
+
+// console.log(typeof num)
+
+
+
+// nullish coalesing
+// console.log(2??2)
+
+
+
+//es2014
+
+"use strict"
+
+var num=90
+console.log(num)
