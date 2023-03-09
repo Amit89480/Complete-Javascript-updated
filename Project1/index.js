b94c431a5ae499f6aae0229bec2286d5f6419d9f
Amit Pandey <access89480@gmail.com>
2023-03-10 00:59:14 +0530

    updated

--- Project1/index.js
+++ Project1/index.js
@@ -5,20 +5,43 @@ var button=document.getElementById('nextJoke');
 
 
 
-const generateJokes=()=>{
+// const generateJokes=()=>{
+//     const header={
+//         headers:{
+//             Accept:"application/json"
+//         }
+//     }
+    
+// fetch('https://icanhazdadjoke.com',header)
+// .then((res)=>res.json())
+// .then((data)=>{
+//     jokes.innerHTML=data.joke;
+// }).catch((error)=>{
+//     console.log(error);
+// })
+   
+
+// }
+
+
+// below code is using async and await
+
+const generateJokes=async()=>{
     const header={
         headers:{
             Accept:"application/json"
         }
     }
+    try{
     
-fetch('https://icanhazdadjoke.com',header)
-.then((res)=>res.json())
-.then((data)=>{
-    jokes.innerHTML=data.joke;
-}).catch((error)=>{
-    console.log(error);
-})
+const response=await fetch('https://icanhazdadjoke.com',header)
+  const data =await response.json();
+
+jokes.innerHTML=data.joke;
+    }catch(error){
+console.log(error);
+    }
+
    
 
 }
